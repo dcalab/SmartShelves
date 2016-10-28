9248593343458b1a2dfd57f6f6ac909d729b83c0
Albert Yerman <ahyerman@umich.edu>
2016-10-28 11:16:32 -0400

    changed table create to now have location table and items table

--- tbl_create.sh
+++ tbl_create.sh
@@ -1,2 +1,3 @@
 mysql -u root -p -e 'CREATE DATABASE SmartShelves'
-mysql -u root -p -e 'CREATE TABLE Items(itemID int PRIMARY KEY AUTO_INCREMENT, name varchar(255), location varchar(255), led int)' SmartShelves
+mysql -u root -p -e 'CREATE TABLE Locations(locationID int PRIMARY KEY AUTO_INCREMENT, name varchar(255), Led int)' SmartShelves
+mysql -u root -p -e 'CREATE TABLE Items(itemID int PRIMARY KEY AUTO_INCREMENT, name varchar(255), locationID int, FOREIGN KEY (locationID) REFERENCES Locations(locationID))' SmartShelves
