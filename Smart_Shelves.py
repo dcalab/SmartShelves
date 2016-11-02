ce3c18e43ac594edc4e67d7078530c95d277b3d9
Albert Yerman <ahyerman@umich.edu>
2016-11-02 13:48:55 -0400

    fixed id problems

--- Smart_Shelves.py
+++ Smart_Shelves.py
@@ -79,7 +79,8 @@ def get_item(item, location, location2):
     end  = location2
     if end:
         checkAndInsertItem(item, start)
-        selectItemId = cur.fetchone()[0]
+        startId = checkAndInsertLocation(start)
+        selectItemId = cur.execute("SELECT itemId FROM Items WHERE name=%s and locationId=%s", (item, startId))
         endId = checkAndInsertLocation(end)
         cur.execute("UPDATE Items SET locationID=%s WHERE ItemID=%s", (endId, selectedItemId))
     else:
