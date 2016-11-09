790db807fcebc63f89b9c7e9468e45d0f4064c68
Jonathan Sussman <jesuss@umich.edu>
2016-11-09 12:35:55 -0500

    fixed reset.sql

--- reset_tbl.sh
+++ reset_tbl.sh
@@ -1,1 +1,2 @@
-mysql -u root -p -e 'DROP TABLE IF EXISTS Items; CREATE TABLE Items(itemID int PRIMARY KEY AUTO_INCREMENT, name varchar(255), location varchar(255), led int)' SmartShelves
+mysql -u root -p -e 'DROP TABLE IF EXISTS Locations; CREATE TABLE Locations(locationID int PRIMARY KEY AUTO_INCREMENT, name varchar(255), Led int)' SmartShelves
+mysql -u root -p -e 'DROP TABLE IF EXISTS Items; CREATE TABLE Items(itemID int PRIMARY KEY AUTO_INCREMENT, name varchar(255), locationID int, FOREIGN KEY (locationID) REFERENCES Locations(locationID))' SmartShelves
