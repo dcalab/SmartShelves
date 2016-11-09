a273126dd2d2cae3a3bc64e5e057412c9f74b3bd
Albert Yerman <ahyerman@umich.edu>
2016-11-09 12:17:08 -0500

    update tbl_update

--- tbl_insert.sql
+++ tbl_insert.sql
@@ -2,33 +2,49 @@ INSERT INTO Locations (name, led)
 VALUES ('unkown', 0);
 
 INSERT INTO Locations (name, led)
-VALUES ('top shelf', 80);
+VALUES ('left side of the top shelf', 1);
 
 INSERT INTO Locations (name, led)
-VALUES ('middle shelf', 50);
+VALUES ('center of the top shelf', 2);
 
 INSERT INTO Locations (name, led)
-VALUES ('bottom shelf', 20);
-
+VALUES ('right side of the top shelf', 3);
 
 INSERT INTO Locations (name, led)
-VALUES ('closet', 2);
+VALUES ('left side of the middle shelf', 4);
+
+INSERT INTO Locations (name, led)
+VALUES ('center of the middle shelf', 5);
+
+INSERT INTO Locations (name, led)
+VALUES ('right side of the middle shelf', 6);
+
+INSERT INTO Locations (name, led)
+VALUES ('left side of the bottom shelf', 7);
+
+INSERT INTO Locations (name, led)
+VALUES ('center of the bottom shelf', 8);
+
+INSERT INTO Locations (name, led)
+VALUES ('right side of the bottom shelf', 9);
+
+INSERT INTO Locations (name, led)
+VALUES ('closet', 0);
 
 INSERT INTO Items (name, locationId)
-VALUES ('paper towel', 4);
-
+VALUES ('paper towel', 2);
 
 INSERT INTO Items (name, locationId)
-VALUES ('cup', 2);
+VALUES ('cup', 5);
 
 INSERT INTO Items(name, locationId)
-VALUES ('shirt', 5);
+VALUES ('shirt', 6);
 
 INSERT INTO Items(name, locationId)
-VALUES ('stapler', 2);
+VALUES ('stapler', 3);
 
 INSERT INTO Items(name, locationId)
-VALUES ('keys', 3);
+VALUES ('keys', 9);
 
 INSERT INTO Items(name, locationId)
 VALUES ('watch', 4);
